a3011e2bd8bcee52daa014ae11c59ffa4ac832cd
bn <parul07hbti>
2022-01-28 21:44:06 +0530

    demo2

---
+++ src/variables/forloop.java
@@ -0,0 +1,22 @@
+package variables;
+
+public class forloop {
+    public static void main(String[] args) {
+//        for (int i=0; i<10; i++){
+//            System.out.println("hellow world "+i);
+//        }
+//        for ( int i=10; i>0; i--){
+//            System.out.println("reverse print"+i);
+//        }
+        for (int i=1; i<=10; i++){
+            int twentyseven=27;
+            int output=twentyseven*i;
+            System.out.println("table of twentyseven="+output);
+        }
+        for (int i=10;i>=0;i--){
+            int four=4;
+            int output=four*i;
+            System.out.println("reverse table of four="+output);
+        }
+    }
+}
