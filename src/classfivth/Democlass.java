6c738399031d9d2d4d456215e7139ef04357b760
bn <parul07hbti>
2022-01-28 20:45:35 +0530

    demo2

---
+++ src/classfivth/Democlass.java
@@ -0,0 +1,11 @@
+package classfivth;
+
+public class Democlass {
+    public static void main(String[] args) {
+        System.out.println("welcome to the world java");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+    }
+}
