971449235fc296b69d780dc0f3539e7a994b3ff0
bn <parul07hbti>
2022-01-30 11:03:25 +0530

    forloop10

--- src/variables/forloop10.java
+++ src/variables/forloop10.java
@@ -8,5 +8,16 @@ public class forloop10 {
         for (int i=10;i>=0;i--){
         System.out.println("parul"+i);
         }
+        for (int i=1; i<=10;i++){
+            int nine=9;
+            int output=nine*i;
+            System.out.println("table of nine"+output);
+        }
+        for (int i=1; i<=10;i++){
+           int nine=9;
+           int output=nine*i;
+            System.out.println("reverse the table nine"+output);
+        }
     }
 }
+
