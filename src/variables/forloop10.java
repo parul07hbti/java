1e3fac3aebee74f42ff4a83624fff43baebb044c
bn <parul07hbti>
2022-01-30 10:48:40 +0530

    forloop10

---
+++ src/variables/forloop10.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class forloop10 {
+    public static void main(String[] args) {
+        for (int i =0; i<20;i++){
+            System.out.println("parul"+i);
+        }
+        for (int i=10;i>=0;i--){
+        System.out.println("parul"+i);
+        }
+    }
+}
